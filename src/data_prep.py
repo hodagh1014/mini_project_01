7f89f0e8ce2f0313c10ae188ed396de2e5810ee1
hodagh1014 <hoda.gh1014@gmail.com>
2026-08-24 11:07:11 +0330

    Complete data preparation functions

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,28 +1,29 @@
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 def Load_data():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(script_dir, '..', 'data', 'creditcard.csv')
-
     df = pd.read_csv(data_path)
-
     return df
 
+def dataset_info(df):
+    print("dataset shape:\n")
+    print(df.shape)
+    print("dataset info:")
+    print(df.info())
 
 def CheckClassDistribution(df):
     class_counts = df["Class"].value_counts()
     class_percentage = df["Class"].value_counts(normalize=True) * 100
 
-    print("Class Counts:")
-    print(class_counts)
-
     print("\nClass Percentage:")
     print(class_percentage)
 
-    return class_counts, class_percentage
+    return  class_percentage
 
 
 def CheckFeature(df):
@@ -32,21 +33,18 @@ def CheckFeature(df):
     print("\nData Types:")
     print(df.dtypes)
 
-    return df.columns, df.dtypes
 
 
 def CheckFeatureAmount(df):
     print("Amount Statistics:")
     print(df["Amount"].describe())
 
-    return df["Amount"].describe()
 
 
 def CheckFeatureTime(df):
     print("Time Statistics:")
     print(df["Time"].describe())
 
-    return df["Time"].describe()
 
 
 def CheckMissingValue(df):
@@ -55,7 +53,6 @@ def CheckMissingValue(df):
     print("Missing Values:")
     print(missing_value)
 
-    return missing_value
 
 
 def CheckDuplicate(df):
@@ -63,20 +60,6 @@ def CheckDuplicate(df):
 
     print("Number of Duplicates:")
     print(duplicate_count)
+    print("remove duplicates:\n",df.drop_duplicates())
 
-    return duplicate_count
-
-
-def SplitData(df):
-    x = df.drop("Class", axis=1)
-    y = df["Class"]
-
-    x_train, x_test, y_train, y_test = train_test_split(
-        x,
-        y,
-        test_size=0.2,
-        stratify=y,
-        random_state=42
-    )
-
-    return x_train, x_test, y_train, y_test
+    return df.drop_duplicates()
